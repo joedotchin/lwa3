9d6bf724ffe4365b6da28ef9585613852031fe1d
Joe Chin <joe.chin@spok.com>
2021-10-17 01:32:57 -0500

    cleanup

--- app.js
+++ app.js
@@ -2,26 +2,9 @@ const express = require('express');
 const app = express();
 const api_helper = require('./API_helper');
 const url = require('url');
-const http = require('http');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/api/info', (req, res) => {
-  res.send({ application: 'lwa3', version: '1.0' });
-});
-app.post('/api/v1/getback', (req, res) => {
-  res.send({ ...req.body });
-});
-
-app.get('/getAPIResponse', (req, res) => {
-  api_helper.make_API_call('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => {
-      res.json(response)
-  })
-  .catch(error => {
-      res.send(error)
-  })
-})
 
 app.get('/api/breaches', (req, res) => {
   const queryObject = url.parse(req.url,true).query;
